110f172a9b91e5f9f574a9f9644bab9c6ebf553a
Deidre Amponsaa <deidre.mensah@gmail.com>
2017-03-23 22:23:50 -0400

    Update clip many features to one boundary.py

--- clip many features to one boundary.py
+++ clip many features to one boundary.py
@@ -7,17 +7,3 @@ boundary =r'L:\Buffalo\Williams_NYRE\Data\Analysis\Data_Routine\Data.gdb\Data\Pr
 xy_tolerance = ""
 for feature in inFeatures:
     arcpy.Clip_analysis(inFeatures, boundary, feature + "_Clip", xy_tolerance)
-    
-inFeatures = ['NWI_Wetlands','dtl_cnty','WBDHU8','WBDHU10','SSURGO','Townships_Merged']
-boundary ='Project_Features_10m_buffer'
-xy_tolerance = ""
-for feature in inFeatures: 
-    arcpy.Clip_analysis(inFeatures, boundary, inFeatures + "_Clip", xy_tolerance)
-    
-for feature in inFeatures: 
-    arcpy.Clip_analysis(inFeatures, boundary, feature + "_Clip", xy_tolerance)
-    
-for feature in inFeatures: 
-    arcpy.Clip_analysis(feature, boundary, feature + "_Clip", xy_tolerance)
-    
-
